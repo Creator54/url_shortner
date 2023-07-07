c3c6ba92d84e8b46bcc669f4063eed5a1921a06a
creator54 <hi.creator54@gmail.com>
2023-07-08 00:29:04 +0530

    convert Database to actual paths pointing to redirects

--- src/main/java/dev/creator54/Database.java
+++ src/main/java/dev/creator54/Database.java
@@ -1,127 +1,87 @@
 package dev.creator54;
 
-import org.json.JSONObject;
-import org.json.JSONTokener;
-
 import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Iterator;
 
 public class Database {
-    private static final String DB_FILE_PATH = System.getProperty("user.dir") + "/db.json";
-    private static File db;
+    private static final String REDIRECTS_FOLDER_PATH = System.getProperty("user.dir") + "/redirects/";
 
-    public Database() throws IOException {
-        db = new File(DB_FILE_PATH);
-        if (!db.exists()) {
-            createEmptyJsonDb();
-        }
-    }
-
-    private void createEmptyJsonDb() throws IOException {
-        try (FileWriter fileWriter = new FileWriter(DB_FILE_PATH)) {
-            JSONObject emptyJson = new JSONObject();
-            fileWriter.write(emptyJson.toString(2));
+    public Database() {
+        File redirectsFolder = new File(REDIRECTS_FOLDER_PATH);
+        if (!redirectsFolder.exists()) {
+            redirectsFolder.mkdirs();
         }
     }
 
     public void add(String key, String value) {
-        try {
-            JSONObject jsonDb = getJsonDb();
+        File[] files = new File(REDIRECTS_FOLDER_PATH).listFiles();
 
-            // Check if the value is already present in the database
-            if (containsValue(jsonDb, value)) {
-                System.out.println(getKey (value) + " -> " + value);
-                return;
-            }
-
-            jsonDb.put(key, value);
-            saveJsonDb(jsonDb);
-            System.out.println(key + " -> " + value);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public String getKey(String value) {
-        try {
-            JSONObject jsonDb = getJsonDb();
-
-            // Iterate through the keys and check for the matching value
-            Iterator<String> keys = jsonDb.keys();
-            while (keys.hasNext()) {
-                String key = keys.next();
-                String storedValue = jsonDb.getString(key);
-                if (storedValue.equals(value)) {
-                    return key; // Found the key associated with the value
+        if (files != null) {
+            for (File file : files) {
+                try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                    String existingValue = reader.readLine();
+                    if (existingValue.equals(value)) {
+                        String existingKey = file.getName();
+                        System.out.println(existingKey + " -> " + value);
+                        return; // Value is already present, do not create a duplicate
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             }
+        }
+
+        String filePath = REDIRECTS_FOLDER_PATH + key;
+        File file = new File(filePath);
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            writer.write("<!DOCTYPE html>\n");
+            writer.write("<html>\n");
+            writer.write("<head>\n");
+            writer.write("  <meta http-equiv=\"refresh\" content=\"0; URL='" + value + "'\" />\n");
+            writer.write("</head>\n");
+            writer.write("<body>\n");
+            writer.write("</body>\n");
+            writer.write("</html>");
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        return null; // Value not found in the database
+        System.out.println(key + " -> " + value);
     }
-
-
-    private boolean containsValue(JSONObject jsonDb, String value) {
-        Iterator<String> keys = jsonDb.keys();
-        while (keys.hasNext()) {
-            String key = keys.next();
-            String storedValue = jsonDb.getString(key);
-            if (storedValue.equals(value)) {
-                return true; // Value is already present
-            }
-        }
-        return false; // Value is not present
-    }
-
 
     public void remove(String keyOrValue) {
-        try {
-            JSONObject jsonDb = getJsonDb();
-            ArrayList<String> keysToRemove = new ArrayList<> ();
-            for (String key : jsonDb.keySet()) {
-                String value = jsonDb.getString(key);
-                if (key.equals(keyOrValue) || value.equals(keyOrValue)) {
-                    keysToRemove.add(key);
+        File[] files = new File(REDIRECTS_FOLDER_PATH).listFiles();
+
+        if (files != null) {
+            boolean removed = false;
+
+            for (File file : files) {
+                String fileName = file.getName();
+                String filePath = file.getAbsolutePath();
+
+                try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                    String value = reader.readLine();
+
+                    if (fileName.equals(keyOrValue) || value.equals(keyOrValue)) {
+                        file.delete();
+                        System.out.println("Removed: " + keyOrValue);
+                        removed = true;
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             }
-            for (String key : keysToRemove) {
-                jsonDb.remove(key);
-            }
-            if (!keysToRemove.isEmpty()) {
-                saveJsonDb(jsonDb);
-                System.out.println("Removed:  " + keyOrValue);
-            } else {
+
+            if (!removed) {
                 System.out.println("Key or value not found in the DB.");
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+        } else {
+            System.out.println("Database is empty.");
         }
     }
 
     public boolean containsKey(String key) {
-        try {
-            JSONObject jsonDb = getJsonDb();
-            return jsonDb.has(key);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return false;
-    }
-
-    private JSONObject getJsonDb() throws IOException {
-        FileInputStream fis = new FileInputStream(db);
-        InputStreamReader isr = new InputStreamReader(fis, StandardCharsets.UTF_8);
-        JSONTokener tokener = new JSONTokener(isr);
-        return new JSONObject(tokener);
-    }
-
-    private void saveJsonDb(JSONObject jsonDb) throws IOException {
-        try (FileWriter fileWriter = new FileWriter(DB_FILE_PATH)) {
-            fileWriter.write(jsonDb.toString(2));
-        }
+        File file = new File(REDIRECTS_FOLDER_PATH + key);
+        return file.exists();
     }
 }
